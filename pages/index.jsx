c5f91ec6769ef5600a2000df96618edec1ea8ea5
Juan Santisteban <juandiegosantisteban115@gmail.com>
2022-10-07 17:57:30 -0500

    commit2

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,6 +4,7 @@ import Layout from '../components/Layout'
 import ProductItem from '../components/ProductItem'
 import styles from '../styles/Home.module.css'
 import data from '../utils/data'
+import Link from 'next/link'
 
 export default function Home() {
 
@@ -15,6 +16,14 @@ export default function Home() {
         <div className="filer_product">
           <h1 className="title_product">Lista de productos</h1>
           <Fliter_Products products={products} />
+          <li className="nav-item">
+            <Link href="/cart">
+              <a className="nav-link active" aria-current="page" href="#">
+                Cart{" "}
+                
+              </a>
+            </Link>
+          </li>
         </div>
 
         <div className="container_card">
